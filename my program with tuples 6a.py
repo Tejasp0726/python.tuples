dd2cf5dfba4a27835ce6f3054f5703bf8fdd5cb3
Tejas P <tejasteju0726@gmail.com>
2024-10-12 18:31:36 +0530

    Add files via upload

---
+++ my program with tuples 6a.py
@@ -0,0 +1,13 @@
+bookstore = ("pen", "penpencil", "refil","sticky notes")
+print(bookstore)
+print(bookstore[0])  
+print(bookstore[-1])  
+print(bookstore[1:3])
+a = (75, 85 ,90 , 25)
+b =(82, 95 ,20 ,65 )
+print(a)
+print(b)
+sumof  = a + b
+print(sumof)
+Repeatit  = (45,54,45) * 3
+print(Repeatit)
